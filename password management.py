cb3972823778b3545b32cebf0bf74429e6265ec7
ashwin-kumar-0-0-8 <112598672+ashwin-kumar-0-0-8@users.noreply.github.com>
2023-07-30 19:28:15 +0530

    password management.py

--- password management.py
+++ password management.py
@@ -1,6 +1,7 @@
-import hashlib
-from cryptography.fernet import Fernet
-from secrets import compare_digest
+import hashlib #used for hashing the master password and later used for comparing
+from cryptography.fernet import Fernet #encryption and decryption processes to ensure the security and reliability of your cryptographic
+from secrets import compare_digest """used to securely compare two strings in constant time, which is crucial in cryptographic 
+                                    applications to avoid potential timing attacks"""
 
 
 master_password=input("Enter the master password : ")
@@ -11,19 +12,18 @@ def ckeck(a):
 def add():
     name=input("Account name : ")
     pwd=input("Password : ")
-    with open(r"C:\Users\KB\OneDrive - Frontera Consulting\Documents\apassword.txt",'a') as f:
+    # where the user id and password is stored
+    with open(r"C:\Users\KB\OneDrive - Frontera Consulting\Documents\apassword.txt",'a') as f: 
         key=Fernet.generate_key()
         fernet = Fernet(key)
         pwd= fernet.encrypt(pwd.encode())
-
-
-
-        f.write(name+"|"+pwd +"|"+key\n")
+        f.write(name+"|"+pwd +"|"+str(key)\n")
 def view():
     
     with open(r"C:\Users\KB\OneDrive - Frontera Consulting\Documents\apassword.txt",'r') as f:
         for line in f.readlines():
             user,passw,key=line.split("|")
+            fernet = Fernet(key)
             passw = fernet.decrypt(passw).decode()
             passw2=passw.rstrip()
             print("User : ",user,"  ,Password : ", passw2)
